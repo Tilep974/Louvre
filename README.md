89a712d60b0504d49434ec0312c0508e7c8c9b5d
Tilep <kevin@lepinay.re>
2018-04-16 14:19:37 +0200

    Creation

---
+++ README.md
@@ -0,0 +1,4 @@
+Louvre
+======
+
+A Symfony project created on April 11, 2018, 9:51 am.
---
+++ src/OC/PlatformBundle/Entity/Ticket.php
@@ -0,0 +1,291 @@
+<?php
+
+namespace OC\PlatformBundle\Entity
+
+use Doctrine\ORM\Mapping as ORM;
+use OC\platformbundle\entity\TicketOrder;
+use Symfony\Component\Validator\Constraints as Assert
+
+/**
+ * Ticket
+ *
+ * @ORM\Table(name="ticket")
+ * @ORM\Entity(repositoryClass="OC\PlatformBundle\Repository\TicketRepository")
+ */
+class Ticket
+{
+	/**
+	 * @var int
+	 *
+	 * @ORM\Column(name="id", type="integer")
+	 * @ORM\Id
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	private $id;
+	
+	/**
+	 * @var string
+	 * 
+	 * @ORM\Column(name="first_name", type="string", length=255)
+	 *@Assert\Type(
+	 *	type="string",
+	 *	message="The first name must be a valid string."
+	 *)
+	 *@Assert\Length(
+	 *	min=2,
+	 *	max=50,
+	 *	minMessage="The first name must have at least 2 characters."
+	 *	maxMessage="The first name must have at most 50 characters."
+	 *)
+	 */
+	private $firstName;
+	
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(name="name", type="string",length=255)
+	 *@Assert\Type(
+	 *	type="string",
+	 *	message="The first name must be a valid string."
+	 *)
+	 *@Assert\Length(
+	 *	min=2,
+	 *	max=50,
+	 *	minMessage="The first name must have at least 2 characters."
+	 *	maxMessage="The first name must have at most 50 characters."
+	 *)
+	 */
+	private $name;
+	
+	/**
+	 * @var int
+	 * 
+	 * @ORM\Column(name="country", type="string", length=255)
+	 *@Assert\Type(
+	 *	type="string",
+	 *	message="The first name must be a valid string."
+	 *)
+	 *@Assert\Length(
+	 *	min=2,
+	 *	max=50,
+	 *	minMessage="The first name must have at least 2 characters."
+	 *	maxMessage="The first name must have at most 50 characters."
+	 *)
+	 */
+	private $country;
+	
+	/**
+	 * @var \DateTime
+	 * 
+	 * @ORM\Column(name="birth_date", type="datetime")
+	 * @Assert\DateTime(message="Birth date must be either a valid DateTime object or a valid date string.")
+	 */
+	private $birthDate;
+	
+	/**
+	 * @var bool
+	 * 
+	 * @ORM\Column(name="discounted", type="boolean")
+	 * @Assert\Type(
+	 * type="bool",
+	 * message="The discount must be a boolean."
+	 * )
+	 */
+	private $discounted;
+	
+	/**
+	 * @var int
+	 * @ORM\Column(name="price", type="integer")
+	 * @Assert\GreaterThanOrEqual(
+	 * 	value=0,
+	 * 	message="The price of the tickets must be equal to or over 0."
+	 * )
+	 */
+	private $price
+	
+	/**
+	 * @ORM\ManytoOne(targetEntity="OC\PlatformBundle\Entity\TicketOrder", inversedBy="tickets")
+	 * @ORM\joincolumn(nullable=false)
+	 * @Assert\Valid()
+	 */
+	private $order;
+	
+	/**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     *
+     * @return Ticket
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Ticket
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set country
+     *
+     * @param string $country
+     *
+     * @return Ticket
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    /**
+     * Get country
+     *
+     * @return string
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * Set birthDate
+     *
+     * @param \DateTime $birthDate
+     *
+     * @return Ticket
+     */
+    public function setBirthDate($birthDate)
+    {
+        $this->birthDate = $birthDate;
+
+        return $this;
+    }
+
+    /**
+     * Get birthDate
+     *
+     * @return \DateTime
+     */
+    public function getBirthDate()
+    {
+        return $this->birthDate;
+    }
+
+    /**
+     * Set discounted
+     *
+     * @param boolean $discounted
+     *
+     * @return Ticket
+     */
+    public function setDiscounted($discounted)
+    {
+        $this->discounted = $discounted;
+
+        return $this;
+    }
+
+    /**
+     * Get discounted
+     *
+     * @return bool
+     */
+    public function getDiscounted()
+    {
+        return $this->discounted;
+    }
+
+    /**
+     * Set price
+     *
+     * @param integer $price
+     *
+     * @return Ticket
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return int
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set order
+     *
+     * @param \OTS\BillingBundle\Entiy\TicketOrder $order
+     *
+     * @return Ticket
+     */
+    public function setOrder(TicketOrder $order)
+    {
+        $this->order = $order;
+
+        return $this;
+    }
+
+    /**
+     * Get order
+     *
+     * @return \OTS\BillingBundle\Entiy\TicketOrder
+     */
+    public function getOrder()
+    {
+        return $this->order;
+    }
+}
+	
